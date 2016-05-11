441c70eb2a9336603fb51ce0dc65438e7e5c0159
Patrick Münch <patrick.muench1111@gmail.com>
2016-05-11 16:41:42 +0200

    add cis docker test 2.3 and 2.4
    Signed-off-by: Patrick Münch <patrick.muench1111@gmail.com>

--- controls/docker_level1.rb
+++ controls/docker_level1.rb
@@ -31,18 +31,40 @@ control 'cis-docker-2.1' do
   ref 'https://docs.docker.com/engine/userguide/networking/default_network/container-communication/'
   ref 'https://entwickler.de/online/development/docker-netzwerk-container-microservices-126443.html'
 
-  describe command('ps aux | grep docker') do
-    its('stdout') { should match (/--icc=false/) }
+  describe json('/etc/docker/daemon.json') do
+    its(['icc']) { should eq(false) }
   end
 end
 
 control 'cis-docker-2.2' do
   impact 1.0
   title 'Set the logging level'
-  desc 'Setting up an appropriate log level, configures the Docker daemon to log events that you would want to review later. A base log level of \'info\' and above would capture all logs except debug logs. Until and unless required, you should not run Docker daemon at \'debug\' log level.'
+  desc 'Setting up an appropriate log level, configures the Docker daemon to log events that you would want to review later. A ase log level of \'info\' and above would capture all logs except debug logs. Until and unless required, you should not run docker daemon at \'debug\' log level.'
   ref 'https://docs.docker.com/engine/reference/commandline/daemon/'
 
-  describe command('ps aux | grep docker') do
-    its('stdout') { should match (/(--log-level="info")|(-l info)/) }
+  describe json('/etc/docker/daemon.json') do
+    its(['log-level']) { should eq('info') }
+  end
+end
+
+control 'cis-docker-2.3' do
+  impact 1.0
+  title 'Allow Docker to make changes to iptables'
+  desc 'Iptables are used to set up, maintain, and inspect the tables of IP packet filter rules in the Linux kernel. Allow the Docker daemon to make changes to the iptables.'
+  ref 'https://docs.docker.com/v1.8/articles/networking/'
+
+  describe json('/etc/docker/daemon.json') do
+    its(['iptables']) { should eq(true) }
+  end
+end
+
+control 'cis-docker-2.4' do
+  impact 1.0
+  title 'Do not use insecure registries'
+  desc 'Docker considers a private registry either secure or insecure. By default, registries are considered secure'
+  ref 'https://docs.docker.com/registry/insecure/'
+
+  describe json('/etc/docker/daemon.json') do
+    its(['insecure-registries']) { should be_empty }
   end
 end
