c641480294814af64ad90adbcfba9f01451a861a
Patrick Münch <patrick.muench1111@gmail.com>
2016-05-11 13:05:11 +0200

    add cis benchmark 1.12 to 1.15
    Signed-off-by: Patrick Münch <patrick.muench1111@gmail.com>

--- controls/docker_level1.rb
+++ controls/docker_level1.rb
@@ -161,3 +161,54 @@ control 'cis-docker-1.11' do
     its(:lines) { should include(rule) }
   end
 end
+
+control 'cis-docker-1.12' do
+  impact 1.0
+  title 'Audit Docker files and directories - /etc/default/docker'
+  desc 'Apart from auditing your regular Linux file system and system calls, audit all Docker related files and directories. Docker daemon runs with \'root\' privileges. Its behavior depends on some key files and directories. /etc/default/docker is one such file. It holds various parameters for Docker daemon. It must be audited, if applicable.'
+  ref 'https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/6/html/Security_Guide/chap-system_auditing.html'
+  ref 'https://docs.docker.com/engine/reference/commandline/daemon/#daemon-configuration-file'
+
+  only_if { os[:family] != 'centos' }
+  describe auditd_rules do
+    its(:lines) { should include('-w /etc/default/docker -p rwxa -k docker') }
+  end
+end
+
+control 'cis-docker-1.13' do
+  impact 1.0
+  title 'Audit Docker files and directories - /etc/docker/key.json'
+  desc 'Apart from auditing your regular Linux file system and system calls, audit all Docker related files and directories. Docker daemon runs with \'root\' privileges. Its behavior depends on some key files and directories. /etc/docker/key.json is one such file. It holds various parameters for Docker daemon. It must be audited, if applicable.'
+  ref 'https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/6/html/Security_Guide/chap-system_auditing.html'
+
+  describe auditd_rules do
+    its(:lines) { should include('-w /etc/docker/key.json -p rwxa -k docker') }
+  end
+end
+
+control 'cis-docker-1.14' do
+  impact 1.0
+  title 'Audit Docker files and directories - /usr/bin/docker-containerd'
+  desc 'Apart from auditing your regular Linux file system and system calls, audit all Docker related files and directories. Docker daemon runs with \'root\' privileges. Its behavior depends on some key files and directories. /usr/bin/docker-containerd is one such file. Docker now relies on containerd and runC to spawn containers. It must be audited, if applicable.'
+  ref 'https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/6/html/Security_Guide/chap-system_auditing.html'
+  ref 'https://github.com/docker/docker/pull/20662'
+  ref 'https://containerd.tools/'
+
+  describe auditd_rules do
+    its(:lines) { should include('-w /usr/bin/docker-containerd -p rwxa -k docker') }
+  end
+end
+
+control 'cis-docker-1.15' do
+  impact 1.0
+  title 'Audit Docker files and directories - /usr/bin/docker-runc'
+  desc 'Apart from auditing your regular Linux file system and system calls, audit all Docker related files and directories. Docker daemon runs with \'root\' privileges. Its behavior depends on some key files and directories. /usr/bin/docker-runc is one such file. Docker now relies on containerd and runC to spawn containers. It must be audited, if applicable.'
+  ref 'https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/6/html/Security_Guide/chap-system_auditing.html'
+  ref 'https://github.com/docker/docker/pull/20662'
+  ref 'https://containerd.tools/'
+  ref 'https://github.com/opencontainers/runc'
+
+  describe auditd_rules do
+    its(:lines) { should include('-w /usr/bin/docker-runc -p rwxa -k docker') }
+  end
+end
