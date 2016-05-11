0600b36c405eaa66fb892e1b19762106922b2fad
Patrick Münch <patrick.muench1111@gmail.com>
2016-05-11 16:41:21 +0200

    correct the cis docker 1.13 test
    Signed-off-by: Patrick Münch <patrick.muench1111@gmail.com>

--- controls/docker_host_os_level1.rb
+++ controls/docker_host_os_level1.rb
@@ -177,12 +177,12 @@ end
 
 control 'cis-docker-1.13' do
   impact 1.0
-  title 'Audit Docker files and directories - /etc/docker/key.json'
-  desc 'Apart from auditing your regular Linux file system and system calls, audit all Docker related files and directories. Docker daemon runs with \'root\' privileges. Its behavior depends on some key files and directories. /etc/docker/key.json is one such file. It holds various parameters for Docker daemon. It must be audited, if applicable.'
+  title 'Audit Docker files and directories - /etc/docker/daemon.json'
+  desc 'Apart from auditing your regular Linux file system and system calls, audit all Docker related files and directories. Docker daemon runs with \'root\' privileges. Its behavior depends on some key files and directories. /etc/docker/daemon.json is one such file. It holds various parameters for Docker daemon. It must be audited, if applicable.'
   ref 'https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/6/html/Security_Guide/chap-system_auditing.html'
 
   describe auditd_rules do
-    its(:lines) { should include('-w /etc/docker/key.json -p rwxa -k docker') }
+    its(:lines) { should include('-w /etc/docker/daemon.json -p rwxa -k docker') }
   end
 end
 
