1c5bcb5c9cf02f27066468e076eeb62666902d34
Patrick Münch <patrick.muench1111@gmail.com>
2016-05-12 16:33:18 +0200

    move cis docker test 2.8 and 2.9 to docker_level2.rb
    Signed-off-by: Patrick Münch <patrick.muench1111@gmail.com>

--- controls/docker_level1.rb
+++ controls/docker_level1.rb
@@ -122,17 +122,3 @@ control 'cis-docker-2.7' do
     its(['default-ulimits', 'nofile']) { should eq('100:200') }
   end
 end
-
-control 'cis-docker-2.8' do
-  impact 1.0
-  title 'Enable user namespace support'
-  desc 'Enable user namespace support in Docker daemon to utilize container user to host user re-mapping. This recommendation is beneficial where containers you are using do not have an explicit container user defined in the container image. If container images that you are using have a pre-defined non-root user, this recommendation may be skipped since this feature is still in its infancy and might give you unpredictable issues and complexities.'
-  ref 'http://man7.org/linux/man-pages/man7/user_namespaces.7.html'
-  ref 'https://docs.docker.com/engine/reference/commandline/daemon/'
-  ref 'http://events.linuxfoundation.org/sites/events/files/slides/User%20Namespaces%20-%20ContainerCon%202015%20-%2016-9-final_0.pdf'
-  ref 'https://github.com/docker/docker/issues/21050'
-
-  describe json('/etc/docker/daemon.json') do
-    its(['userns-remap']) { should eq('default') }
-  end
-end
--- controls/docker_level2.rb
+++ controls/docker_level2.rb
@@ -23,3 +23,28 @@ title 'CIS Docker Benchmark - Level 2 - Docker'
 only_if do
   command('docker').exist?
 end
+
+control 'cis-docker-2.8' do
+  impact 1.0
+  title 'Enable user namespace support'
+  desc 'Enable user namespace support in Docker daemon to utilize container user to host user re-mapping. This recommendation is beneficial where containers you are using do not have an explicit container user defined in the container image. If container images that you are using have a pre-defined non-root user, this recommendation may be skipped since this feature is still in its infancy and might give you unpredictable issues and complexities.'
+  ref 'http://man7.org/linux/man-pages/man7/user_namespaces.7.html'
+  ref 'https://docs.docker.com/engine/reference/commandline/daemon/'
+  ref 'http://events.linuxfoundation.org/sites/events/files/slides/User%20Namespaces%20-%20ContainerCon%202015%20-%2016-9-final_0.pdf'
+  ref 'https://github.com/docker/docker/issues/21050'
+
+  describe json('/etc/docker/daemon.json') do
+    its(['userns-remap']) { should eq('default') }
+  end
+end
+
+control 'cis-docker-2.9' do
+  impact 1.0
+  title 'Confirm default cgroup usage'
+  desc 'The --cgroup-parent option allows you to set the default cgroup parent to use for all the containers. If there is no specific use case, this setting should be left at its default.'
+  ref 'https://docs.docker.com/engine/reference/commandline/daemon/'
+
+  describe json('/etc/docker/daemon.json') do
+    its(['cgroup-parent']) { should eq('docker') }
+  end
+end
